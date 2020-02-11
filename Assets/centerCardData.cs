95dc34ded54e69021bccf8b059995ed8e6e0257c
dankam66 <60823706+dankam66@users.noreply.github.com>
2020-02-11 10:25:30 +0000

    Scriptable Object for cards
    property, tile and center cards

---
+++ Assets/centerCardData.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Center Card")]
+public class CardData : ScriptableObject{
+
+	public string cardName;
+	public enum cardType {OppertunityKnocks,PotLuck};
+	public cardType cardtype;
+
+	public string actionText;
+	public enum actionType {payment,movement,outOfJail,goToJail};
+	public enum participantType {player,allOtherPlayers,bank};
+
+	public participantType leftParticipant; //in "player pays 50 to bank", this is player
+	public actionType actiontype; //in "player pays 50 to bank", this is payment
+	public string actionValue; //in "player pays 50 to bank", this is 50
+	public participantType rightParticipant; //in "player pays 50 to bank", this is bank
+
+	//maybe put this in the monobehaviour type script
+	public void action(){
+		//switch statement for actionType, LevelManager calls this to do the card action
+		//for any payment ones, the actionValue will have to be changed to a int
+		switch (actiontype){
+			case actionType.goToJail:
+				//player gets sent to the jail, no collecting money
+				break;
+			case actionType.outOfJail:
+				//get a card that can be kept and used if in jail
+				break;
+			case actionType.movement:
+				//to move forward + integer, to move backwards - integer; moving forwards mean able to collect money
+				break;
+			case actionType.payment:
+				// leftParticipant pays rightParticipant actionValue amount of money
+				break;
+		}	
+	}
+}
---
+++ Assets/tileCardData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Tile Card", menuName = "Tile Card")]
+public class tileCardData : ScriptableObject {
+
+	public string cardName;
+	public string cardColour;
+	public int cardPosition; //position on board
+	public enum tileType {property,train,utility,other} //train comes under property
+	public tileType tiletype;
+
+	public enum rentTypes {baseCost,oneHouse,twoHouses,threeHouses,fourHouses,hotel,twoTrains,threeTrains,fourTrains};
+	public Dictionary<rentTypes,int> rentPrices; //set as 'null' for non-rentable tile's, or leave empty?
+
+	//maybe put this in the monobehaviour type script
+	public void action(){
+		switch (tiletype) {
+		case tileType.property:
+			//
+			break;
+		}
+	}
+}
